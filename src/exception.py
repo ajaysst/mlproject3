5fd7232e121399ffbc4b14432fd48d3ff123142e
ajatsst <ajay843@gmail.com>
2025-08-24 01:06:47 +0530

    EDA and problems stage

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_exc_tb=error_detail.exc_info()
@@ -16,4 +17,6 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message_detail=error_detail)
 
     def __str__(self):
-        return self.error_message    
+        return self.error_message   
+
+   
